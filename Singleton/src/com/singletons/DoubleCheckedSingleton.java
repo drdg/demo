ee3a627b5050cbd2742dfca857b51bce9cd1eed3
drdg <drdg5307397@qq.com>
2013-03-31 17:37:18 +0800

    注释 2013年3月31日 17:37:16

--- Singleton/src/com/singletons/DoubleCheckedSingleton.java
+++ Singleton/src/com/singletons/DoubleCheckedSingleton.java
@@ -1,3 +1,13 @@
+/**
+ *写到这里，面试官可能仍然会狡猾的看了你一眼，
+ *继续刁难到：这个写法有没有什么性能问题呢？
+ *答案肯定是有的！
+ *同步的代价必然会一定程度的使程序的并发度降低。
+ *那么有没有什么方法，一方面是线程安全的，有可以有很高的并发度呢？
+ *我们观察到，线程不安全的原因其实是在初始化对象的时候，
+ *所以，可以想办法把同步的粒度降低，只在初始化对象的时候进行同步。
+ *这里有必要提出一种新的设计思想——双重检查锁（Double-Checked Lock）。
+ */
 package com.singletons;
 public final class DoubleCheckedSingleton {
 	private static DoubleCheckedSingleton singObj = null;
--- Singleton/src/com/singletons/EagerSingleton.java
+++ Singleton/src/com/singletons/EagerSingleton.java
@@ -1,11 +1,10 @@
-package com.singletons;
 /**
  * 面试的时候，常常会被问到这样一个问题：
  * 请您写出一个单例模式（Singleton Pattern）吧。
  * 好吧，写就写，这还不容易。顺手写一个：
  * @author Administrator
  */
-
+package com.singletons;
 public final class EagerSingleton {
 	private static EagerSingleton singObj = new EagerSingleton();
 
